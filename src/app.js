d66938114a5d56961ec0f8c0150a90a384909524
murphy <1243917465@qq.com>
2022-03-02 15:51:04 +0800

    创建 button-group

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Button from './button'
+import ButtonGroup from './button-group'
 import Icon from './icon'
 
 Vue.component('wu-button', Button)
+Vue.component('wu-button-group', ButtonGroup)
 Vue.component('wu-icon', Icon)
 new Vue({
     el: '#app',
     data: {
-        loading1: true
+        loading1: true,
+        loading2: false
     }
 })
---
+++ src/button-group.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="button-group">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.button-group {
+  display: inline-flex;
+  vertical-align: middle;
+
+  > .wu-button {
+    margin-right: -1px;
+    &:first-child {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+    &:last-child {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    }
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
